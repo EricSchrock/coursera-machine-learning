738831b75500a7294d28060613dc310b1514bc64
Eric Schrock <eric.schrock22@gmail.com>
2019-06-11 22:14:36 -0400

    Exercise 1 - optional part

--- Linear_Regression/ex1/computeCostMulti.m
+++ Linear_Regression/ex1/computeCostMulti.m
@@ -13,9 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+% Both are equivalent
+%J = (1 / (2 * m)) * sum(((X * theta) - y) .^ 2);
+J = (1 / (2 * m)) * ((X * theta) - y)' * ((X * theta) - y);
 
 % =========================================================================
 
--- Linear_Regression/ex1/ex1_multi.m
+++ Linear_Regression/ex1/ex1_multi.m
@@ -82,8 +82,8 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
-num_iters = 400;
+alpha = 1.0;
+num_iters = 50;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -104,8 +104,10 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
 
+Xex_Norm = ([1 1650 3] - [0 mu]) ./ [1 sigma];
+
+price = Xex_Norm * theta;
 
 % ============================================================
 
@@ -149,8 +151,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
 
+price = [1 1650 3] * theta;
 
 % ============================================================
 
